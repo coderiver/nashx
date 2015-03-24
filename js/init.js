50e83abd0fe26b2f8e86e44c26a1983d9df265c0
etna-scorpio <etna.scorpio@gmail.com>
2015-03-24 14:29:41 +0200

    fixes 2

--- js/init.js
+++ js/init.js
@@ -1,5 +1,6 @@
 head.load("js/lib/jquery.js", 
 					"js/lib/cycle.js",
+					"js/lib/cycle.carousel.js",
 					"js/lib/datepicker.js",
 					"js/lib/jquery.fancybox.js",
 					"js/lib/jquery.fancybox.pack.js",
